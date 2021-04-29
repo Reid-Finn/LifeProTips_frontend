18e0718dd77407c002facb80b1fff9effcf576e0
Reid <Reid.Finn@yahoo.com>
2021-04-29 14:08:00 -0500

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './App.css';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import lifeprotipsReducer from './reducers/LifeprotipsReducer'
+
+const store = createStore(lifeprotipsReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
